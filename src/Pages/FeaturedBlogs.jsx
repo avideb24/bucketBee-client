0ae9e46261977ba6314e442f327b03618ff92a7d
avideb24 <debavi24@gmail.com>
2024-02-19 15:43:16 +0600

    animated cursor added on featured page

--- src/Pages/FeaturedBlogs.jsx
+++ src/Pages/FeaturedBlogs.jsx
@@ -49,6 +49,20 @@ const FeaturedBlogs = () => {
 
     return (
         <div>
+             <AnimatedCursor
+                innerSize={15}
+                outerSize={35}
+                innerScale={1}
+                outerScale={2}
+                outerAlpha={0}
+                hasBlendMode={true}
+                innerStyle={{
+                    backgroundColor: '#eab308'
+                }}
+                outerStyle={{
+                    border: '3px solid #eab308'
+                }}
+            />
             <Helmet>
                 <title>Featured Blogs</title>
                 <link rel="icon" href={favicon} />
